b7c097c3ec7f60144e5192e32891817346af6fd6
juneikon <136241918+juneikon@users.noreply.github.com>
2023-06-21 03:21:42 -0400

    Add files via upload

---
+++ onkm2.js
@@ -0,0 +1,23 @@
+var typed = new Typed('.multiple-text', {
+    strings: [
+      'STEM Student',
+      'Student at BLDEA',
+      'Ex Student at AITM',
+      
+    ],
+    
+  
+    
+    typeSpeed: 100,
+  
+    backSpeed: 100,
+    backDelay:1000,
+  
+    
+    loop:true
+  
+    
+    
+  });
+
+  
